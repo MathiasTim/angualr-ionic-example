c270bca0596c02a3782a22b774e6ae1aae8fed73
Mathias Maier <m.maier@mwaysolutions.com>
2015-04-24 00:04:38 +0200

    added some comments and minor improvements

--- app/main/controllers/start-ctrl.js
+++ app/main/controllers/start-ctrl.js
@@ -8,20 +8,28 @@ angular.module('main')
 
   console.log('Hello from your Controller: StartCtrl in module main:. This is your scope:', this);
 
+  // use this function to add the desired amount
   this.addAmount = function () {
+    if (!this.amount) {
+      return; // leave method when amount is empty
+    }
+
+    // use a simple js array
     this.data.expenses.push(
       {
         amount: this.amount,
-        tracked: new Date().getTime()
+        tracked: new Date().getTime() // add a timestamp in milliseconds
       }
     );
-    this.amount = '';
+    this.amount = null; // to clear the amount field after the add
   };
 
+  // this method should calculate the total expeneses
   this.total = function () {
     var total = 0;
+    // go through each item and add its amount to the total
     angular.forEach(this.data.expenses, function (item) {
-      total += item.amount;
+      total +=  isNaN(item.amount) ? 0 : item.amount;
     });
     return total;
   };
