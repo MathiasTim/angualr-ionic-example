0f4a7c320bf875b70be977510b3e6fb365441635
Mathias Maier <m.maier@mwaysolutions.com>
2015-05-31 14:41:50 +0200

    updated readme, added comment

--- app/main/controllers/start-ctrl.js
+++ app/main/controllers/start-ctrl.js
@@ -37,6 +37,7 @@ angular.module('main')
     return total;
   };
 
+  // this method searches the category by id so we can display the correct icon
   this.getCategory = function (id) {
     var category = null;
     angular.forEach(this.categories, function (item) {
